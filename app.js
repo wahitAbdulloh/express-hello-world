c004be51da77c2dea91bc5d4dfb2e095a721b138
wahitAbdulloh <wahid4bdullah@gmail.com>
2022-11-12 10:00:02 +0700

    add todos applications

--- app.js
+++ app.js
@@ -1,34 +1,71 @@
-const express = require('express')
-const path = require("path");
-const app = express()
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ extended: false }));
 
 // #############################################################################
 // Logs all request paths and method
-app.use(function (req, res, next) {
-  res.set('x-timestamp', Date.now())
-  res.set('x-powered-by', 'cyclic.sh')
-  console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
-  next();
+app.use((req, res, next) => {
+    res.set('x-timestamp', Date.now());
+    res.set('x-powered-by', 'cyclic.sh');
+    console.log(`[${new Date().toISOString()}] ${req.ip} ${req.method} ${req.path}`);
+    next();
 });
 
+const todos = [];
+
 app.get('/', (req, res) => {
-  res.json({tes:'hello world'});
+    res.json({ statusCode: 200, message: `Welcome on ${process.env.HOME_NAME}` });
+});
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.get('/todos/:id', (req, res) => {
+    const { params } = req;
+    res.json(todos.filter((todo) => todo.id === params.id)[0] || {});
+});
+
+app.post('/todos', (req, res) => {
+    const { body } = req;
+    body.id = uuidv4();
+    todos.push(body);
+    res.json(body);
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const { params, body } = req;
+    const todoUpdateIndex = todos.findIndex((todo) => todo.id === params.id);
+    todos[todoUpdateIndex] = { ...body, id: params.id };
+
+    res.json(body);
+});
+
+app.delete('/todos/:id', (req, res) => {
+    const { params } = req;
+    const todoDeleteIndex = todos.findIndex((todo) => todo.id === params.id);
+    todos.splice(todoDeleteIndex, 1);
+    res.json({ status: 'success', message: `todos with id ${params.id} has been removed` });
 });
 
 // #############################################################################
 // Catch all handler for all other request.
-app.use('*', (req,res) => {
-  res.json({
-      at: new Date().toISOString(),
-      method: req.method,
-      hostname: req.hostname,
-      ip: req.ip,
-      query: req.query,
-      headers: req.headers,
-      cookies: req.cookies,
-      params: req.params
+app.use('*', (req, res) => {
+    res.json({
+        at: new Date().toISOString(),
+        method: req.method,
+        hostname: req.hostname,
+        ip: req.ip,
+        query: req.query,
+        headers: req.headers,
+        cookies: req.cookies,
+        params: req.params
     })
-    .end()
-})
+        .end();
+});
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const app = require('./app')
+require('dotenv').config();
+const app = require('./app');
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
